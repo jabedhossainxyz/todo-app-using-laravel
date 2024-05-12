051beb063c9e4d97c684b7312c7d274baf631f66
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-05-13 00:04:50 +0600

    Update environment variables, remove unnecessary imports, and modify TodoController delete method
    * Update .env.example with APP_ENV=production, APP_DEBUG=false, and APP_URL=http://localhost
    * Remove

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -77,18 +77,14 @@ class TodoController extends Controller
     }
     public function destroy($id)
     {
-        // Find the todo by id
         $todo = Todos::find($id);
 
         if (!$todo) {
-            // Handle if the todo is not found
             abort(404);
         }
 
-        // Delete the todo
         $todo->delete();
 
-        // Redirect back with a success message
         return redirect()->route('todos.index')->with('alert-success', 'Todo deleted successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
